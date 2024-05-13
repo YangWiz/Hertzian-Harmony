ab563477216a127e5237d0351e57883618850be7
YangHoo <polymagicyang@protonmail.com>
2024-05-13 17:05:40 +0200

    format

--- main.py
+++ main.py
@@ -38,17 +38,20 @@ db_url = db_url.replace('postgres', 'postgresql')
 
 psql = db(db_url)
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/api/new_phone/{phone}")
 def add_new_phone(phone: str, db: Session = Depends(psql.connect)):
-    new_phone = models.PhonePool(phone = phone)
+    new_phone = models.PhonePool(phone=phone)
     db.add(new_phone)
 
     db.commit()
@@ -59,27 +62,44 @@ def add_new_phone(phone: str, db: Session = Depends(psql.connect)):
 
     return new_phone
 
+
 @app.get("/api/all_phones")
 def all_phones(db: Session = Depends(psql.connect)):
     return db.query(models.PhonePool).all()
 
+
 @app.get("/api/free_phones")
 def free_phones(db: Session = Depends(psql.connect)):
-    return db.query(models.PhonePool).filter(models.PhonePool.question_type == None).all()
+    return db.query(models.PhonePool).filter(
+        models.PhonePool.question_type is None).all()
+
 
 @app.get("/api/vote/{number}")
 def vote(number: str, db: Session = Depends(psql.connect)):
-    phone = db.query(models.PhonePool).filter(models.PhonePool.phone == number).one_or_none()
+    phone = db.query(models.PhonePool).filter(
+        models.PhonePool.phone == number).one_or_none()
     if phone is None:
         return
 
     if phone.question_type == "yes":
-        question = db.query(models.Questions).filter(models.Questions.voteYesPhone == number).one_or_none()
-        qbuilder = QuestionBuilder(question.yes + 1, question.no, heroku_url, question.uuid)
+        question = db.query(
+            models.Questions).filter(
+            models.Questions.voteYesPhone == number).one_or_none()
+        qbuilder = QuestionBuilder(
+            question.yes + 1,
+            question.no,
+            heroku_url,
+            question.uuid)
         question.yes += 1
     else:
-        question = db.query(models.Questions).filter(models.Questions.voteNoPhone == number).one_or_none()
-        qbuilder = QuestionBuilder(question.yes, question.no + 1, heroku_url, question.uuid)
+        question = db.query(
+            models.Questions).filter(
+            models.Questions.voteNoPhone == number).one_or_none()
+        qbuilder = QuestionBuilder(
+            question.yes,
+            question.no + 1,
+            heroku_url,
+            question.uuid)
         question.no += 1
 
     qbuilder.commit()
@@ -89,9 +109,11 @@ def vote(number: str, db: Session = Depends(psql.connect)):
 
     return question
 
+
 @app.get("/api/questions")
 def all_questions(db: Session = Depends(psql.connect)):
     return db.query(models.Questions).all()
+
 
 @app.post("/api/question")
 def add_question(question: Question, db: Session = Depends(psql.connect)):
@@ -102,13 +124,13 @@ def add_question(question: Question, db: Session = Depends(psql.connect)):
     quuid = uuid.uuid4()
     yes, no = phones
     question = models.Questions(
-        prompt = question.description,
-        uuid = str(quuid),
-        url = heroku_url + "vxml/" + str(quuid) + ".xml",
-        voteYesPhone = yes.phone,
-        voteNoPhone = no.phone,
-        yes = 0,
-        no = 0,
+        prompt=question.description,
+        uuid=str(quuid),
+        url=heroku_url + "vxml/" + str(quuid) + ".xml",
+        voteYesPhone=yes.phone,
+        voteNoPhone=no.phone,
+        yes=0,
+        no=0,
     )
 
     qbuilder = QuestionBuilder(0, 0, heroku_url, str(quuid))
@@ -130,16 +152,24 @@ def add_question(question: Question, db: Session = Depends(psql.connect)):
     db.add(question)
     db.commit()
     db.refresh(question)
-    return question 
+    return question
+
 
 def get_free_phones(db: Session = Depends(psql.connect)):
-    phones = db.query(models.PhonePool).filter(models.PhonePool.question_uuid == None).all()
+    phones = db.query(models.PhonePool).filter(
+        models.PhonePool.question_uuid is None).all()
     if (len(phones) <= 1):
-        return [] 
+        return []
     else:
         return [phones[0], phones[1]]
 
-def combine_phone_question(phone: models.PhonePool, quuid: str, qtype: str, db: Session = Depends(psql.connect)):
+
+def combine_phone_question(
+    phone: models.PhonePool,
+    quuid: str,
+    qtype: str,
+    db: Session = Depends(
+        psql.connect)):
     setattr(phone, "question_uuid", quuid)
     setattr(phone, "question_type", qtype)
 
@@ -147,11 +177,13 @@ def combine_phone_question(phone: models.PhonePool, quuid: str, qtype: str, db: 
     db.commit()
     db.refresh(phone)
 
+
 @app.get("/vxml/{path}", response_class=FileResponse)
 def fetch_files(path: str):
     # Decode URL-encoded characters
     decoded_string = urllib.parse.unquote(path)
     return "vxml/" + decoded_string
+
 
 @app.get("/audios/{path}", response_class=FileResponse)
 def fetch_files(path: str):
@@ -159,12 +191,24 @@ def fetch_files(path: str):
     decoded_string = urllib.parse.unquote(path)
     return "audios/" + decoded_string
 
+
 @app.post("/api/question/{language}")
-def add_question(language: str, question: Question, db: Session = Depends(psql.connect)):
+def add_question(
+    language: str,
+    question: Question,
+    db: Session = Depends(
+        psql.connect)):
     prompt = question.description
     id = question.uuid
 
     tts = ICT4DTTS(openai_api=openai_api)
-    tts.english_text_to_speech(prompt, language, is_generate_wav_file=True, file_path="audios/" + str(id) + "-" + language + ".wav")
+    tts.english_text_to_speech(
+        prompt,
+        language,
+        is_generate_wav_file=True,
+        file_path="audios/" +
+        str(id) +
+        "-" +
+        language +
+        ".wav")
     return language + " Added"
-
